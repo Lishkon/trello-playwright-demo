6aaedb0bfae5136de5f62ee5b55fd74eb52ee09d
Alex Lychak <lishkon@gmail.com>
2025-12-08 23:19:56 -0500

    Adding a separate reporter for CI

--- playwright.config.js
+++ playwright.config.js
@@ -23,11 +23,12 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [
-  ['list'],
-  ['junit', { outputFile: 'reports/junit/results.xml' }],
-  ['html',  { outputFolder: 'playwright-report', open: 'never' }]
-],
+  // reporter: [
+  //   ['list'],
+  //   ['junit', { outputFile: 'reports/junit/results.xml' }],
+  //   ['html',  { outputFolder: 'playwright-report', open: 'never' }]
+  // ],
+  reporter: process.env.CI ? 'dot' : 'list',
 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
