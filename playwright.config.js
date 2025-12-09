1f2d018db51cb5f41288885b36636269ea30f56e
Alex Lychak <lishkon@gmail.com>
2025-12-09 00:48:18 -0500

    Fixing the config for the CI report

--- playwright.config.js
+++ playwright.config.js
@@ -2,14 +2,6 @@
 import { defineConfig, devices } from '@playwright/test';
 
 /**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
-
-/**
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
@@ -21,14 +13,12 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 2 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  // reporter: [
-  //   ['list'],
-  //   ['junit', { outputFile: 'reports/junit/results.xml' }],
-  //   ['html',  { outputFolder: 'playwright-report', open: 'never' }]
-  // ],
-  reporter: process.env.CI ? 'dot' : 'list',
+  
+  reporter: process.env.CI
+    ? [['dot'], ['html']]
+    : [['list'], ['html']],
 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
@@ -79,12 +69,5 @@ export default defineConfig({
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
   ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
 
