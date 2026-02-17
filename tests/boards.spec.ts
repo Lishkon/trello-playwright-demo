c8a657f189bfb3f1a282d201907df5eed1e18fc2
Alex Lychak <lishkon@gmail.com>
2026-02-17 00:07:04 -0500

    feat: implement post-test activity to cleanup created boards
    Add API request interaction method to remove the newly created boards to stay within the limit of 7 new created boards allowed by the Trello's free plan.
    Configure CICD pipeline to enable flawless test execution in the Pipeline.

--- tests/boards.spec.ts
+++ tests/boards.spec.ts
@@ -5,15 +5,47 @@ import { URL } from '../data/constants';
 
 let boardsPage: Boards;
 
+// Used for Board clean up after each test
+let shortBoardId: string;
+
 test.describe("Sample test for Boards page", () => {
     test.beforeEach(async ({ page }) => {
         boardsPage = new Boards(page);
         await page.goto(URL.E2E.PROD);
     })
 
-    test("Creating a board", async ({}) => {
+    /**
+     * Pre-requisites:
+     * 
+     * 1. API Key — go to https://trello.com/power-ups/admin, 
+     *    click on your Power-Up or create one, and you'll find your API key
+     * 2. API Token — on the same page where you see the key, there's a link to 
+     *    generate a token. It will ask you to authorize access to your account
+     * 
+     * For local testing: 
+     * Add the generated API key and Token to the TRELLO_API_KEY and TRELLO_API_TOKEN in the .env file
+     * 
+     * For CI/CD: 
+     * 1. Add the TRELLO_API_KEY and TRELLO_API_TOKEN as CI/CD variables in CI/CD Settings -> Variables.
+     *    Keep the variables Protected when running the pipeline in the Main branch, but unprotect for the other branches
+     * 2. Add the TRELLO_API_KEY and TRELLO_API_TOKEN to .gitlab-ci.yml into the variables section:
+     *        TRELLO_API_KEY: ${TRELLO_API_KEY}
+     *        TRELLO_API_TOKEN: ${TRELLO_API_TOKEN}
+     */
+    test.afterEach(async ({request }) => {
+        if (shortBoardId) {
+            await request.delete(
+                `https://api.trello.com/1/boards/${shortBoardId}?key=${process.env.TRELLO_API_KEY}&token=${process.env.TRELLO_API_TOKEN}`
+            );
+        }
+    })
+
+    test("Creating a board", async ({page}) => {
         let boardName = faker.company.buzzAdjective() + " board";
         await boardsPage.createBoard(boardName);
-        expect(await boardsPage.getCurrentCompanydName()).toBe(boardName);
+        await expect(boardsPage.currentBoardSelector).toContainText(boardName);
+        // Storing the boardId locally to be able to clean up the board after the test
+        shortBoardId = page.url().split('/')[4]; 
+        console.log(`The newly created board id: ${shortBoardId}`);
     })
 })
